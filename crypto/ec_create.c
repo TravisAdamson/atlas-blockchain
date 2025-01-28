8bcb85ac71b2a98ff3a61daba879f9ab38eaa542
Travis <travis.adamson@atlasschool.com>
2025-01-28 10:21:52 -0600

    Trying task 1

---
+++ crypto/ec_create.c
@@ -0,0 +1,15 @@
+#include "hblk_crypto.h"
+
+/**
+ * ec_create - Creates a new keyed pair using secp256k1 elliptic curve
+ * Return: Pointer to EC Key struct or NULL
+ */
+EC_KEY *ec_create(void)
+{
+	EC_KEY *key = NULL;
+
+	key = EC_KEY_new_by_curve_name(NID_secp256k1);
+	if (!key)
+		return (NULL);
+	return (EC_KEY_generate_key(key) ? key : NULL);
+}
--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -18,7 +18,14 @@
 #include <openssl/bio.h>
 #include <openssl/ecdh.h>
 
+#define EC_CURVE NID_secp256k1
+#define EC_PUB_LEN 65
+#define SIG_MAX_LEN    72
+#define PRI_FILENAME   "key.pem"
+#define PUB_FILENAME   "key_pub.pem"
+
 uint8_t *sha256(int8_t const *s, size_t len,
 				uint8_t digest[SHA256_DIGEST_LENGTH]);
+EC_KEY *ec_create(void);
 
 #endif
